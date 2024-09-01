13f8e3fc27cbe19b28a6edc6bf021af72aff4262
dou <15309487104@163.com>
2024-09-01 20:56:49 +0800

    lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -5,7 +5,7 @@ import java.util.Formatter;
  * with a large number of additional methods.
  *
  * @author P. N. Hilfinger, with some modifications by Josh Hug and melaniecebula
- *         [Do not modify this file.]
+ * [Do not modify this file.]
  */
 public class IntList {
     /**
@@ -29,7 +29,7 @@ public class IntList {
      * A List with null rest, and first = 0.
      */
     public IntList() {
-    /* NOTE: public IntList () { }  would also work. */
+        /* NOTE: public IntList () { }  would also work. */
         this(0, null);
     }
 
@@ -82,7 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList desc = A;
+        while (desc.rest != null) {
+            desc = desc.rest;
+        }
+        desc.rest = B;
+        return A;
     }
 
     /**
@@ -91,22 +96,18 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList result = new IntList(A.first, null);
+        IntList pointer = result;
+
+        A = A.rest;
+        while (A != null) {
+            pointer.rest = new IntList(A.first, null);
+            pointer = pointer.rest;
+            A = A.rest;
+        }
+        pointer.rest = B;
+        return result;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     /**
